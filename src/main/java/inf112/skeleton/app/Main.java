eccf7579200217bede048ed67f8fcac9d9c30259
Kasper.Treborg <kasper.treborg@student.uib.no>
2023-03-10 10:26:28 +0100

    tester push en gang til en gang til

--- src/main/java/inf112/skeleton/app/Main.java
+++ src/main/java/inf112/skeleton/app/Main.java
@@ -3,7 +3,7 @@ package inf112.skeleton.app;
 public class Main {
 
 	public static void main(String[] args) {
-		// Test på push en gang til
+		// Test på push en gang til en gang til
 		App.startIt(args);
 
 	}
