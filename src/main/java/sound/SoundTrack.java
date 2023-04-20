f702333e01b28ed7740f3278660d298be0875aa0
bastiansunde <bastian.sunde@student.uib.no>
2023-04-20 16:17:24 +0800

    litt feil i metode. men commit for å vise at jeg holder på og ikke søver.

--- src/main/java/sound/SoundTrack.java
+++ src/main/java/sound/SoundTrack.java
@@ -1,6 +1,31 @@
 package sound;
+import javafx.sound.*;
 
 public class SoundTrack {
+    private Clip clip;
 
+    public SoundTrack(String audioFile) {
+
+        URL url = getClass().getResource(("") + audioFile);
+        AudioInputStream audioInputStream = audioSystem.getaudioInputStream(url);
+        clip = Audiosyste.getClip();
+        clip.open(audioInputStream);
+        }
+    {
+
+    }   // A block of code that can take inn a audio file, need to find a fitting sound, by using URL
+
+
+    public void play(){
+        if (clip != null && !clip.isRunning()) {
+            clip.start();
+            }
+        }
+        public void stop() {
+        if (clip != null) {
+            clip.loop(Clip.LOOP_CONTINUOUSLY);
+        }
+
+    }
 
 }
