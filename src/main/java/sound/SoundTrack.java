cbbfb6a65b4bca5b9c93064bfa46dbc35f391110
bastiansunde <bastian.sunde@student.uib.no>
2023-04-20 16:59:51 +0800

    Må bruke Javafx, ser ut til at intellJ sliter med og fatte dette. så for forstett og pøve og der etter inmplimentere in game

--- src/main/java/sound/SoundTrack.java
+++ src/main/java/sound/SoundTrack.java
@@ -1,31 +1,52 @@
 package sound;
-import javafx.sound.*;
+
+import javafx.scene.media.AudioClip;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
 
 public class SoundTrack {
+
+    private MediaPlayer mediaPlayer;
     private Clip clip;
 
     public SoundTrack(String audioFile) {
+        Media sound = new Media(getClass().getResource(("") + audioFile.toString()));
+        mediaPlayer = new MediaPlayer(sound);
 
+/*
         URL url = getClass().getResource(("") + audioFile);
         AudioInputStream audioInputStream = audioSystem.getaudioInputStream(url);
         clip = Audiosyste.getClip();
         clip.open(audioInputStream);
+
+ */
+
         }
     {
 
-    }   // A block of code that can take inn a audio file, need to find a fitting sound, by using URL
+    }   /* A block of code that can take inn a audio file, need to find a fitting sound, second one uses
+     javax. This was wrong use same format as the application.
+    */ 
 
 
     public void play(){
-        if (clip != null && !clip.isRunning()) {
+        mediaPlayer.play();
+        /*if (clip != null && !clip.isRunning()) {
             clip.start();
-            }
+            }*/
         }
         public void stop() {
+        mediaPlayer.stop();
+        /*
         if (clip != null) {
             clip.loop(Clip.LOOP_CONTINUOUSLY);
+         */
         }
 
+        public void loop(){
+        mediaPlayer.setCycleCount(mediaPlayer.indefinite);
+        mediaPlayer.paly();
     }
 
 }
