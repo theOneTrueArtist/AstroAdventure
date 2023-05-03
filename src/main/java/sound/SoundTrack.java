b94f62dad9072cb15795654b859a469fee90a28d
bastiansunde <bastian.sunde@student.uib.no>
2023-05-04 01:02:54 +0300

    Ikke så mye frem gang men det er mye å få styr på. Må finne ut hver jeg kan implimentere lyden i selve spillet

--- src/main/java/sound/SoundTrack.java
+++ src/main/java/sound/SoundTrack.java
@@ -1,52 +1,38 @@
 package sound;
 
-//import javafx.scene.media.AudioClip;
-//import javafx.scene.media.Media;
-//import javafx.scene.media.MediaPlayer;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 
 public class SoundTrack {
 
-    //private MediaPlayer mediaPlayer;
-    //private Clip clip;
+    private MediaPlayer mediaPlayer;
+    private Clip clip;
 
     public SoundTrack(String audioFile) {
-        //Media sound = new Media(getClass().getResource(("") + audioFile.toString()));
-        //mediaPlayer = new MediaPlayer(sound);
+        Media sound = new Media(getClass().getResource(("") + audioFile.toString()));
+        mediaPlayer = new MediaPlayer(sound);
 
-/*
-        URL url = getClass().getResource(("") + audioFile);
-        AudioInputStream audioInputStream = audioSystem.getaudioInputStream(url);
-        clip = Audiosyste.getClip();
-        clip.open(audioInputStream);
+/
 
- */
 
-        }
+    }
+
     {
 
     }   /* A block of code that can take inn a audio file, need to find a fitting sound, second one uses
      javax. This was wrong use same format as the application.
-    */ 
+    */
 
 
-    public void play(){
-        //mediaPlayer.play();
-        /*if (clip != null && !clip.isRunning()) {
-            clip.start();
-            }*/
-        }
-        public void stop() {
-        //mediaPlayer.stop();
-        /*
-        if (clip != null) {
-            clip.loop(Clip.LOOP_CONTINUOUSLY);
-         */
+    public void stop() {
+        mediaPlayer.stop();
+
+
+        public void loop () {
+            mediaPlayer.setCycleCount(mediaPlayer.indefinite);
+            mediaPlayer.paly();
         }
 
-        public void loop(){
-        //mediaPlayer.setCycleCount(mediaPlayer.indefinite);
-        //mediaPlayer.paly();
     }
-
 }
